8fa4ecd7fb08a029632f9a2e91582cb19a9e7c4b
Li Xi <lxi1@binghamton.edu>
2023-02-22 11:50:57 -0500

    Delete extra file

--- src/python/crawl.py
+++
@@ -1,6 +0,0 @@
-import csv
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen('https://my.nycha.info/DevPortal/Portal/SelectDevelopment/261')
-bs = BeautifulSoup(html, 'html.parser')
