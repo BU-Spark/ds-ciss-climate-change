a4cceed87ae9c3814f47c53680f584e69ddaab3a
Daniel Tse <danielts@bu.edu>
2023-02-21 19:05:17 -0500

    have proof of concept for retrieving html of demographic and household tabs

--- src/python/myNYCHA.py
+++ src/python/myNYCHA.py
@@ -2,22 +2,27 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# initialize selenium
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
-
 driver = webdriver.Chrome()
 driver.maximize_window()
+
+# fetch html for single building's portal
 driver.get("https://my.nycha.info/DevPortal/Portal/SelectDevelopment/261")
 
-# navChildren = driver.find_elements(By.XPATH,"//ul[@id='nav']")
-# print(navChildren)
-
+# navigate to the development data for that building
 devData = driver.find_element(By.LINK_TEXT,'Development Data')
 devData.click()
-input()
 
-# URL = "https://my.nycha.info/DevPortal/Portal/SelectDevelopment/180"
-# page = requests.get(URL,verify=False)
+# navigate to demographics tab
+demographics = driver.find_element(By.XPATH,"//html").get_attribute('outerHTML')
+demographics = driver.find_element(By.LINK_TEXT,'Demographics')
+demographics.click()
 
-# print(page.text)
+# scrape the demographics data
+
+
+
+input() # so that the window doesn't close
