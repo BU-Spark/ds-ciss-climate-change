cc08ed19cc2f1fb9ab40492cb984f494a8ce6928
zoexili <lxi1@binghamton.edu>
2023-02-23 18:10:42 -0500

    cleaned the html tags in csv file

--- src/python/crawl.py
+++ src/python/crawl.py
@@ -1,6 +1,38 @@
-import csv
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import csv
+import ssl
 
-html = urlopen('https://my.nycha.info/DevPortal/Portal/SelectDevelopment/261')
-bs = BeautifulSoup(html, 'html.parser')
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+# bs = BeautifulSoup(html, 'html.parser')
+# bs = BeautifulSoup(html.read(), 'html.parser')
+# html = urlopen('https://www.pythonscraping.com/pages/warandpeace.html')
+# bs = BeautifulSoup(html.read(), 'html.parser')
+
+# nameList = bs.findAll('span', {'class': 'green'})
+# for name in nameList:
+#   print(name.get_text)
+
+# html = urlopen('https://www.pythonscraping.com/pages/page3.html')
+# bs = BeautifulSoup(html, 'html.parser')
+# print(bs.find('img', {'src': '../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
+
+# html = urlopen('https://my.nycha.info/DevPortal/Portal/SelectDevelopment/261')
+# bs = BeautifulSoup(html, 'html.parser')
+# print(bs.get_text)
+# for a in bs.find_all(href=True):
+#     print("Found the URL:", a['href'])
+
+
+
+
+# csvFile = open('sample.csv', 'a')
+# writer = csv.writer(csvFile)
+# csvRow = []
+# try:
+#   csvRow.append(bs.find('img', {'src': '../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
+#   writer.writerow(csvRow)
+# finally:
+#   csvFile.close()
--- src/python/myNYCHA.py
+++ src/python/myNYCHA.py
@@ -3,6 +3,8 @@ from scrape import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import csv
+from bs4 import BeautifulSoup as soup
+
 
 # initialize selenium
 from selenium.webdriver.chrome.options import Options
@@ -15,41 +17,40 @@ driver.maximize_window()
 driver.get("https://my.nycha.info/DevPortal/Portal/SelectDevelopment/261")
 
 # navigate to the development data for that building
-devData = driver.find_element(By.LINK_TEXT,'Development Data')
+devData = driver.find_element(By.LINK_TEXT, 'Development Data')
 devData.click()
 
 # scrape the demographics data
-age_tables = driver.find_element(By.ID,'tab_demographics').find_elements(By.XPATH,".//*") # puts each table in a list
-# print(age_tables[0])
-# print(scrape_table(age_tables[0]))
+# age_tables = driver.find_element(By.ID, 'tab_demographics').find_elements(By.XPATH, ".//*")  # puts each table in a list
 
-# initialize the csvFile
+table_demo = soup(driver.page_source, 'html.parser').find('div', {'id':'tab_demographics'}).text
+table_income = soup(driver.page_source, 'html.parser').find('div', {'id':'tab_household_income'}).text
+
+
 csvFile = open('editors.csv', 'wt+')
+# csvFile = open('editors.csv', 'a')
+
 writer = csv.writer(csvFile)
-csvRow = ['NYCHA']
+csvRow = []
 try:
+    csvRow.append(table_demo)
+    writer.writerow(csvRow)
+    csvRow = []
+    csvRow.append(table_income)
     writer.writerow(csvRow)
 finally:
     csvFile.close()
 
-for table in age_tables:
-    scrape_table(table)
 
-input() # so that the window doesn't close
 
+input()  # so that the window doesn't close
+
+# print(scrape_table(age_tables[0]))
+# for table in age_tables:
+#     scrape_table(table)
 
 # print(age_tables[0].get_attribute('outerHTML'))
 # print(age_tables)
 # scrape_table(age_tables[0])
 # tbl = age_tables[0].get_attribute('outerHTML')
-
-# csvFile = open('editors.csv', 'wt+')
-# writer = csv.writer(csvFile)
-# csvRow = []
-# try:
-#     csvRow.append(tbl)
-#     writer.writerow(csvRow)
-# finally:
-#     csvFile.close()
-
 
