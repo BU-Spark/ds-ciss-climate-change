380b85b56dc708688f2469640cd3c0699427f875
Daniel Tse <danielts@bu.edu>
2023-03-18 17:44:24 -0400

    iteration through every pr's articles, text analysis started

--- src/python/runner.py
+++ src/python/runner.py
@@ -1,4 +1,4 @@
-import selenium
+import re
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 
 URL = "http://nychanow.nyc/issues/"
 
+""" initializes selenium"""
 def init():
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
@@ -13,6 +14,21 @@ def init():
     driver.maximize_window()
     return driver
 
+""" performs text analysis of given article """
+def analyze_article(article,driver:webdriver.Chrome):
+    driver.get(article)
+    # text in divided into various p tags, so we need all of them
+    p_tags = driver.find_element(By.TAG_NAME,"article").find_elements(By.TAG_NAME,"p")
+    text = ""
+    for p in p_tags:
+        # if p tag has text, remove punc and add it 
+        if p.text and not p.text.isspace():
+            stripped_text = re.sub(r'[^\w\s]','',p.text)
+            text += " " + stripped_text
+    # check if text contains any of the NYCHA buildings
+    
+
+""" the worker function """
 def __main__():
     driver = init()
     driver.get(URL)
@@ -20,7 +36,14 @@ def __main__():
 
     releases = driver.find_elements(By.TAG_NAME,"ul")[1].find_elements(By.TAG_NAME,"li") # list of all press releases
     urls = [release.find_element(By.TAG_NAME,"a").get_attribute("href") for release in releases] # store each url in list
-    for url in urls: # navigate through each url
-        driver.get(url)
+    for url in urls: # start scraping each month's pr
+        driver.get(url) # grab html of pr
+        time.sleep(3)
+        # fetch all the links on the page
+        articles = driver.find_elements(By.TAG_NAME,"article")
+        articles = [article.find_element(By.TAG_NAME,"a").get_attribute("href") for article in articles]
+        # for each link, analyze the text for mentions of NYCHA buildings
+        for article in articles:
+            analyze_article(article,driver)
 
 __main__()
