30b60fc272787ebc4448515cf0a7c9ee333da663
Daniel Tse <danielts@bu.edu>
2023-03-19 20:41:02 -0400

    detects mentions of buildings per article - conflicts with names though

--- src/python/runner.py
+++ src/python/runner.py
@@ -1,10 +1,12 @@
 import re
 import time
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 URL = "http://nychanow.nyc/issues/"
+NYCHA_DATA = "src/data/NYCHA.csv"
 
 """ initializes selenium"""
 def init():
@@ -14,25 +16,36 @@ def init():
     driver.maximize_window()
     return driver
 
+""" returns list of NYCHA buildings """
+def fetch_buildings(file):
+    df = pd.read_csv(file)
+    df["1"] = df["1"].str.lower() 
+    building_names = df.iloc[:,1].tolist()
+    return building_names
+
 """ performs text analysis of given article """
-def analyze_article(article,driver:webdriver.Chrome):
+def analyze_article(article,buildings,driver:webdriver.Chrome):
+    print(article)
     driver.get(article)
-    # text in divided into various p tags, so we need all of them
+    # text is divided into various p tags, so we need all of them
     p_tags = driver.find_element(By.TAG_NAME,"article").find_elements(By.TAG_NAME,"p")
     text = ""
     for p in p_tags:
         # if p tag has text, remove punc and add it 
         if p.text and not p.text.isspace():
             stripped_text = re.sub(r'[^\w\s]','',p.text)
-            text += " " + stripped_text
+            text += " " + stripped_text.lower()
     # check if text contains any of the NYCHA buildings
-    
+    for building in buildings:
+        if building in text:
+            print(building,"was mentioned")
 
 """ the worker function """
 def __main__():
     driver = init()
     driver.get(URL)
     time.sleep(5)
+    buildings = fetch_buildings(NYCHA_DATA)
 
     releases = driver.find_elements(By.TAG_NAME,"ul")[1].find_elements(By.TAG_NAME,"li") # list of all press releases
     urls = [release.find_element(By.TAG_NAME,"a").get_attribute("href") for release in releases] # store each url in list
@@ -44,6 +57,11 @@ def __main__():
         articles = [article.find_element(By.TAG_NAME,"a").get_attribute("href") for article in articles]
         # for each link, analyze the text for mentions of NYCHA buildings
         for article in articles:
-            analyze_article(article,driver)
+            analyze_article(article,buildings,driver)
+            time.sleep(5)
+
+""" for specific cases """
+def unit_test():
+    return
 
 __main__()
