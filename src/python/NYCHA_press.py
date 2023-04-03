b0d8bc45056fb6654494068ab61e71d0cad7e487
zoexili <lxi1@binghamton.edu>
2023-04-02 22:48:06 -0400

    extract more accurate start/end dates and not limited to the NYCHA building list, exported to csv

--- src/python/NYCHA_press.py
+++ src/python/NYCHA_press.py
@@ -5,11 +5,57 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup as soup
+import csv
+
 
 URL = "https://www.nyc.gov/site/nycha/about/press/press-releases.page"
 NYCHA_DATA = "python/NYCHA.csv"
+keywords = [
+    "energy",
+    "heating",
+    "water",
+    "heat",
+    "boilers",
+    "air",
+    "sustainability",
+    "reduce",
+    "efficiency",
+    "hot",
+    "lighting",
+    "quality",
+    "oil",
+    "steam",
+    "costs",
+    "savings",
+    "boiler",
+    "service",
+    "emissions",
+    "cost",
+    "repairs",
+    "repair",
+    "performance",
+    "greenhouse",
+    "gas",
+    "commitment",
+    "conservation",
+    "largest",
+    "upgrade",
+    "apartments",
+    "replacement",
+    "fuel",
+    "carbon",
+    "initiative",
+    "power",
+    "reduction",
+    "climate",
+    "services",
+    "homes",
+    "environmental"
+]
 
 """ initializes selenium"""
+
+
 def init():
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
@@ -19,6 +65,8 @@ def init():
 
 
 """ returns list of NYCHA buildings """
+
+
 def fetch_buildings(file):
     df = pd.read_csv(file)
     # Convert Building Address to lower case, and then to a list
@@ -28,19 +76,25 @@ def fetch_buildings(file):
     # print(building_names)
 
 
-""" extract all mentioned dates in article using regex """
-def extract_dates(text):
-    # ooks for dates in the format of "Month Day, Year" or "Day Month Year" and also four-digit years. 
-    dates = re.findall(
-        r'(?:\d{1,2} )?(?:Jan|Feb|Mar|Apr|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]* (?:\d{1,2}, )?\d{2,4}', text)
-    dates += re.findall(r'\d{4}', text)
-    # save dates to a set to avoid duplicates
-    return set(dates)
+""" extract start date and end date of projects in article using regex """
 
+
+def extract_start_dates(text):
+    startDate = re.findall(
+        r'(?: started in |began in | began at | began at | initiated at | launched in | launched at | kicked off in | kicked off at | established in)\d{4}|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]* \d{4}', text)
+    return startDate
+
+
+def extract_end_dates(text):
+    endDate = re.findall(
+        r'(?: ended in | completed in |finished in | constructed in | built in | upgraded in | repaired in | wrapped up in | wrapped up at | terminated in | ended at | completed at | finished at | constructed at | built at | upgraded at | repaired at | terminated at | renovated at | renovated in )\d{4}|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]* \d{4}', text)
+    return endDate
 
 
 """ performs text analysis of given article """
-def analyze_article(url, buildings, driver: webdriver.Chrome):
+
+
+def analyze_article(url, driver: webdriver.Chrome):
     # fetch html for press release portal
     driver.get(url)
     # navigate to the class containing press articles
@@ -56,22 +110,43 @@ def analyze_article(url, buildings, driver: webdriver.Chrome):
     link_list = link_list[: len(link_list) - k]
     # print(link_list)
 
-    # click each article link
-    for name in link_list:
-        driver.get(URL)
-        devData = driver.find_element(By.LINK_TEXT, name)
-        devData.click()
-        p_text = ""
-        for p in soup(driver.page_source, 'html.parser').find_all('p'):
-            stripped_text = re.sub(r'[^\w\s]', '', p.text)
-            p_text += " " + stripped_text.lower()
-        # check if text contains any of the NYCHA buildings
-        mentioned = []
-        for building in buildings:
-            if building in p_text:
-                mentioned.append(building)
-        if mentioned:
-            dates = extract_dates(p_text)
+    csvFile = open('press.csv', 'wt+')
+    writer = csv.writer(csvFile)
+    writer.writerow(["Article Name", "Keywords",
+                    "Start Date", "End Date", "Project"])
+    try:
+        # click each article link
+        for name in link_list:
+            driver.get(URL)
+            devData = driver.find_element(By.LINK_TEXT, name)
+            devData.click()
+            p_text = ""
+            for p in soup(driver.page_source, 'html.parser').find_all('p'):
+                stripped_text = re.sub(r'[^\w\s]', '', p.text)
+                p_text += " " + stripped_text.lower()
+            # check if text contains any of the NYCHA buildings
+            article_name = []
+            keys = []
+            startDate = []
+            endDate = []
+            data = []
+            for key in keywords:
+                # match whole words only from keywords list
+                if re.search(r'\b' + re.escape(key) + r'\b', name, re.IGNORECASE):
+                    print("keyword:", key)
+                    print("Article:", name)
+                    keys.append(key)
+                    article_name.append(name)
+                    startDate = extract_start_dates(p_text)
+                    print("Start Date:", startDate)
+                    endDate = extract_end_dates(p_text)
+                    print("End Date:", endDate)
+                    data = [article_name, keys, startDate, endDate]
+            if startDate or endDate:
+                writer.writerow(data)
+    finally:
+        csvFile.close()
+
     input()  # so that the window doesn't close
 
 
@@ -79,16 +154,15 @@ def __main__():
     driver = init()
     driver.get(URL)
     time.sleep(5)
-    buildings = fetch_buildings(NYCHA_DATA)
-    # this function only work the current url
-    analyze_article(URL, buildings, driver)
+    # buildings = fetch_buildings(NYCHA_DATA)
+    # this function only work with the current 2021-2023 press url
+    analyze_article(URL, driver)
 
 
 __main__()
 
-# What I completed: 1. Scrape date info from the current URL for 2021-2023 press releases.
-# 2. Saved building mentioned and dates.
-# Next Steps: 1. Save building and dates side by side in csv file.
+# What I completed: 1. Exported 2021-2023 presses' start/end dates, project, article, and
+# keywords into csv file. 2. Modified extract_dates function
+# Next Steps: 1. Discuss with clients about the keyword list.
 # 2. scrape data from 2010-2020 press releases.
-# 3. extract_dates(text) function may need modification to suit for press scraping.
-# 4. Scrape capital projects
+# 3. Scrape capital projects
